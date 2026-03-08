116f574660c9d6c781c86df0664701128cee967a
Emma Rucay <emmarucay2006@gmail.com>
2026-03-08 09:26:11 +0100

    Add Struct and manager

---
+++ Tempora/src/models.rs
@@ -0,0 +1,111 @@
+use chrono::{ NaiveDateTime, Duration};
+//NaiveDateTime, it's used to have a date and time without worrying about the time zone
+//Duration, used to have a duration of, say, 30 minutes, 1 hour
+use std::mem::discriminant;
+
+///Main structure to manage tasks and events
+#[derive(Debug, PartialEq)]
+pub struct Manager
+{
+    pub tasks: Vec<Task>,
+    pub events: Vec<Event>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Priority
+{
+    One = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Task
+{
+    pub name: String,
+    pub description: String,
+    pub duration: Duration,
+    pub priority : Priority,
+    pub deadline: NaiveDateTime,
+    pub categories: Vec<String>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Event
+{
+    pub name: String,
+    pub description: String,
+    pub start: NaiveDateTime,
+    pub end: NaiveDateTime,
+    pub priority: Priority,
+    pub categories: Vec<String>,
+}
+
+
+impl Manager
+{
+    pub fn new() -> Self
+    {
+        Self
+        {
+            tasks: Vec::new(),
+            events: Vec::new(),
+        }
+    }
+    ///Adds a new task to manager
+    pub fn add_task(&mut self, task: Task)
+    {
+        self.tasks.push(task);
+    }
+
+    ///Adds a new event to manager
+    pub fn add_event(&mut self, event: Event) -> Result<(), String>
+    {
+        if event.end > event.start
+        {
+            self.events.push(event);
+            Ok(())
+        }
+        else
+        {
+            Err("The event must end after it has started.".to_string())
+        }
+    }
+
+    pub fn display_tasks(&self)
+    {
+        for (i, task) in self.tasks.iter().enumerate()
+        {
+            println!("{}: {} (Priority: {:?}) - Due: {}", i+1, task.name, task.priority, task.deadline);
+        }
+    }
+
+
+    ///Return a list of tasks with a specific priority
+    pub fn get_tasks_by_priority(&self, priority: Priority) -> Vec<&Task>
+    {
+        self.tasks.iter().filter(|t| std::mem::discriminant(&t.priority) == std::mem::discriminant(&priority)).collect()
+    }
+
+    ///Return a list of tasks with a specific deadline
+    pub fn get_tasks_before_deadline(&self, date: NaiveDateTime) -> Vec<&Task>
+    {
+        self.tasks
+            .iter()
+            .filter(|t| t.deadline <= date)
+            .collect()
+    }
+    
+    ///Returns tasks that are due in the next hour
+    pub fn get_upcoming_tasks(&self, now: NaiveDateTime) -> Vec<&Task>
+    {
+        let one_hour_late = now + Duration::hours(1);
+        self.tasks
+            .iter()
+            .filter(|t| t.deadline > now && t.deadline <= one_hour_late)
+            .collect()
+    }
+}
+
